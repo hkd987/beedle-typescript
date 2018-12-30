00b351bd62a795fb9e99d854c209a865f8b69158
Lundin Matthews <lundinmatthews@me.com>
2018-12-29 22:45:19 -0600

    beedle in typescript

--- type/beedle.ts
+++ type/beedle.ts
@@ -1,40 +1,53 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Store {
-    constructor(params) {
-        // Add some default objects to hold our actions, mutations and state
-        this.actions = {};
-        this.mutations = {};
-        this.state = {};
-        this.status = 'resting';
-        this.callbacks = [];
+interface initParams {
+    actions: object,
+    initialState: object,
+    mutations: object,
+}
+
+export default class Store {
+    // Add some default objects to hold our actions, mutations and state
+    private actions: any = {}
+    private mutations: any = {}
+    private state: any = {}
+    private status: string = 'resting'
+    private callbacks: any[] = []
+
+    constructor(params: initParams) {
         const self = this;
+
         // Look in the passed params object for actions and mutations 
         // that might have been passed in
-        if (params && params.actions) {
+        if(params && params.actions) {
             self.actions = params.actions;
         }
-        if (params.hasOwnProperty('mutations')) {
+        
+        if(params.hasOwnProperty('mutations')) {
             self.mutations = params.mutations;
         }
+
         // Set our state to be a Proxy. We are setting the default state by 
         // checking the params and defaulting to an empty object if no default 
         // state is passed in
         self.state = new Proxy((params.initialState || {}), {
-            set(state, key, value) {
+            set(state: any, key: any, value: any) {
+                
                 // Set the value as we would normally
                 state[key] = value;
+
                 // Fire off our callback processor because if there's listeners, 
                 // they're going to want to know that something has changed
                 self.processCallbacks(self.state);
+                
                 // Reset the status ready for the next operation
                 self.status = 'resting';
+                
                 return true;
             }
         });
     }
+
     /**
-     * A dispatcher for actions that looks in the actions
+     * A dispatcher for actions that looks in the actions 
      * collection and runs the action if it can find it
      *
      * @param {string} actionKey
@@ -42,22 +55,28 @@ class Store {
      * @returns {boolean}
      * @memberof Store
      */
-    dispatch(actionKey, payload) {
+    dispatch(actionKey: string, payload: any): boolean {
+  
         const self = this;
+        
         // Run a quick check to see if the action actually exists
         // before we try to run it
-        if (typeof self.actions[actionKey] !== 'function') {
+        if(typeof self.actions[actionKey] !== 'function') {
             console.error(`Action "${actionKey}" doesn't exist.`);
             return false;
         }
+        
         // Let anything that's watching the status know that we're dispatching an action
         self.status = 'action';
+        
         // Actually call the action and pass it the Store context and whatever payload was passed
         self.actions[actionKey](self, payload);
+
         return true;
     }
+
     /**
-     * Look for a mutation and modify the state object
+     * Look for a mutation and modify the state object 
      * if that mutation exists by calling it
      *
      * @param {string} mutationKey
@@ -65,39 +84,49 @@ class Store {
      * @returns {boolean}
      * @memberof Store
      */
-    commit(mutationKey, payload) {
+    commit(mutationKey: string, payload: any): boolean {
         const self = this;
+        
         // Run a quick check to see if this mutation actually exists
         // before trying to run it
-        if (typeof self.mutations[mutationKey] !== 'function') {
+        if(typeof self.mutations[mutationKey] !== 'function') {
             console.error(`Mutation "${mutationKey}" doesn't exist`);
             return false;
         }
+        
         // Let anything that's watching the status know that we're mutating state
         self.status = 'mutation';
+        
         // Get a new version of the state by running the mutation and storing the result of it
         let newState = self.mutations[mutationKey](self.state, payload);
+        
         // Merge the old and new together to create a new state and set it
         self.state = Object.assign(self.state, newState);
+
         return true;
     }
+
     /**
      * Fire off each callback that's run whenever the state changes
      * We pass in some data as the one and only parameter.
      * Returns a boolean depending if callbacks were found or not
-     *
+     * 
      * @param {object} data
      * @returns {boolean}
      */
-    processCallbacks(data) {
+    processCallbacks(data: any): boolean {
         const self = this;
-        if (!self.callbacks.length) {
+    
+        if(!self.callbacks.length) {
             return false;
         }
+        
         // We've got callbacks, so loop each one and fire it off
-        self.callbacks.forEach(callback => callback(data));
+        self.callbacks.forEach(callback => callback(data));    
+
         return true;
     }
+
     /**
      * Allow an outside entity to subscribe to state changes with a valid callback.
      * Returns boolean based on wether or not the callback was added to the collection
@@ -105,16 +134,17 @@ class Store {
      * @param {function} callback
      * @returns {boolean}
      */
-    subscribe(callback) {
+    subscribe(callback: any): boolean {
         const self = this;
-        if (typeof callback !== 'function') {
+
+        if(typeof callback !== 'function') {
             console.error('You can only subscribe to Store changes with a valid function');
             return false;
         }
+        
         // A valid function, so it belongs in our collection
         self.callbacks.push(callback);
+
         return true;
     }
 }
-exports.default = Store;
-//# sourceMappingURL=beedle.js.map
